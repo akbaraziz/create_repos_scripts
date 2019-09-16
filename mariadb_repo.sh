5a608b7a249171ebb866c329a4551c1dcbfb95fc
Akbar Aziz <akbar.aziz@nutanix.com>
2019-09-16 18:31:45 -0500

    Updated Scripts

--- mariadb_repo.sh
+++ mariadb_repo.sh
@@ -8,5 +8,5 @@ cat >/etc/yum.repos.d/MariaDB.repo <<EOL
 name = MariaDB
 baseurl = http://yum.mariadb.org/10.3/centos7-amd64
 gpgkey=https://yum.mariadb.org/RPM-GPG-KEY-MariaDB
-gpgcheck=1
+gpgcheck=0
 EOL
